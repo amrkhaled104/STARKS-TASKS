069eabfcb1a4558251606b13486512dea16c7201
amr <amrkhaledelsisy@gmail.com>
2024-03-13 17:28:38 +0200

    taks for week7

---
+++ week7/task2/2.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#include "task2.h"
+int main ()
+ {
+    printf("sum = %d \nsub = %d\nmul = %d\ndiv = %f",sum(5,4),sub(5,4),mul(5,4),(float)div(5,4));  
+ }
---
+++ week7/task3/task3.h
@@ -0,0 +1,10 @@
+#ifndef TASK3
+#define TASK3
+
+#define Print()  printf(" size of array:- ");
+#define Print1()  printf(" the element of aaray:-\n ");
+#define Array_sum(arr,n)  ({int sum=0;for(int i=0;i<n;i++){sum+=arr[i];}sum;})\
+        
+#endif		
+	
+	
