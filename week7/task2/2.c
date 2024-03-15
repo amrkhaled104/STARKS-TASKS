4bc8626d3570bfb5a2f237f2b12d550d1a89a025
amr <amrkhaledelsisy@gmail.com>
2024-03-15 15:57:47 +0200

    edit task2

--- week7/task2/2.c
+++ week7/task2/2.c
@@ -2,5 +2,41 @@
 #include "task2.h"
 int main ()
  {
-    printf("sum = %d \nsub = %d\nmul = %d\ndiv = %f",sum(5,4),sub(5,4),mul(5,4),(float)div(5,4));  
+	 while(1)
+	 {
+		float a,b; 
+ 
+         printf("enter two number: ");scanf("%g %g",&a,&b);
+         printf("+ -->sum\n- -->sub\n* -->mul\n/ -->div\n");
+         char ch;scanf(" %c",&ch);
+		 switch(ch)
+		 {
+			 
+			 case '+': 
+			 printf("sum = %g \n",sum(a,b)); 
+			 break;
+			 case '-':
+			  printf("sub = %g\n",sub(a,b)); 
+			  break;
+			  case '*':
+			   printf("mul = %g\n",mul(a,b)); 
+			   break;
+			   case '/':
+			   if(a>b)
+			   {
+				    printf("div = %g\n",(float)div(a,b)); 
+			   }
+			   else
+			   {
+				   printf("error");
+			   }
+			   break;
+			   default :
+			   printf("wrong");
+			   break;
+		 }
+	
+	 } 
+         
+ 
  }
