f9914ca5080c8003716ab49a4e0d9fba4588ce7a
amr <amrkhaledelsisy@gmail.com>
2024-04-25 16:40:41 +0200

    Stack With Array

---
+++ DATASTRUCTRE/stack/stack.c
@@ -0,0 +1,51 @@
+
+#include<stdio.h>
+#include "main.h"
+#include "stack.h"
+void createstack(stack *ps)
+{
+	ps->Top=0;
+}
+
+void push(Structentry e , stack *ps)
+{
+	ps->entry[ps->Top++]=e;
+}
+
+void pop(Structentry *pe,stack *ps)
+{
+	*pe=ps->entry[--ps->Top];
+}
+
+void stackfull(stack *ps)
+{
+	return ps->Top==MAXSTACK;
+}
+
+void stackempty(stack *ps)
+{
+	return !ps->Top;
+}
+
+void stacktop(Structentry *pe,stack *ps)
+{
+	*pe=ps->entry[ps->Top-1];
+}
+
+int stacksize(Structentry *pe,stack *ps)
+{
+	return ps->Top;
+}
+
+void clearstack(stack *ps)
+{
+	ps->Top=0;
+}
+
+void traversestack(stack *ps,void(*pf)(Structentry))
+{
+	for(int i=ps->Top;i>0;i--)
+	{
+		*pf(ps->entry[i-1]);
+	}	
+}
